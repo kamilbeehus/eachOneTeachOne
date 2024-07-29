2fde991c4a6abad98cc8e6dc28c46e5977838aa7
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-07-29 16:43:18 +0200

    Configured CORS on Express setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express, { response } from "express";
+import cors from 'cors';
 import { PORT } from "./config.js"
 
 const app = express();
 
+app.use(cors());
+
 app.get('/', (request, response) => {
     console.log(request)
-    return response.status(234).send(
-        "Welcome to MERN Stack Tutorial")
+    return response.status(200).send("Welcome to MERN Stack Tutorial")
 })
 
 app.listen(PORT, () => {
