f5a22a439fd47c926ecfd8350852e5f238b7785e
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-07-30 11:08:59 +0200

    Updated routing setup with express

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
-import express, { response } from "express";
+import express from "express";
 import cors from 'cors';
 import { PORT } from "./config.js"
 
 const app = express();
 
+// Middleware to parse the request body as JSON
 app.use(cors());
 
+// Route for the Landing page 
 app.get('/', (request, response) => {
     console.log(request)
-    return response.status(200).send("Welcome to MERN Stack Tutorial")
+    return response.status(200).send("<h1>Welcome to Each One Teach One</h1>")
+})
+
+// Route for the Login page
+app.get('/auth/login', (request, response) => {
+    return response.status(200).send("Login page")
+})
+
+// Route for handling unvalid requests
+app.get('/*', (request, response) => {
+    return response.status(404).send("Page Not Found")
 })
 
 app.listen(PORT, () => {
