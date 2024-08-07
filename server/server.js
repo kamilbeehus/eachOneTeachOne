7c84f97ce4429d079592880435b08ca587e26cc1
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-08-07 15:32:14 +0200

    Updated server.js with routes and connectDB instances

--- server/server.js
+++ server/server.js
@@ -1,28 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from 'cors';
-import { PORT } from "./config.js"
+import connectDB from './db/connection.js';
+import routes from './routes/index.js';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware to parse the request body as JSON
-app.use(cors());
+app.use(express.json());
 
-// Route for the Landing page 
-app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(200).send("<h1>Welcome to Each One Teach One</h1>")
-})
-
-// Route for the Login page
-app.get('/auth/login', (request, response) => {
-    return response.status(200).send("Login page")
-})
-
-// Route for handling unvalid requests
-app.get('/*', (request, response) => {
-    return response.status(404).send("Page Not Found")
-})
+// Middleware to allow cross-origin requests
+app.use('/', routes);
 
 app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
-})
+    console.log(`Server running on port: ${PORT}`)
+});
