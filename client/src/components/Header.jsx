512b86987a90f3a80bec5583854f8af5976e51a2
Kamil <kamilbeehus179@gmail.com>
2024-08-08 15:13:36 +0200

    Created the signup and login pages but I get an error and I do know why

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,28 @@
-import { NavLink } from "react-router-dom";
-export default function Header() {
-    return (
-        <div>
-            <nav className="flex justify-between items-center mb-6">
-                <NavLink to="/">
-                    <button className="btn btn-primary">how a Dashboard might look like
-                    </button>
-                </NavLink>
+import {Link} from 'react-router-dom';
 
-                <NavLink to="/login">
-                    <button className="btn btn-primary">how a Login Page might look like
-                    </button>
-                </NavLink>
-            </nav>
+export default function Header({
+    heading,
+    paragraph,
+    linkName,
+    linkUrl="#"
+}){
+    return(
+        <div className="mb-10">
+            <div className="flex justify-center">
+                <img 
+                    alt=""
+                    className="h-14 w-14"
+                    src="https://ik.imagekit.io/pibjyepn7p9/Lilac_Navy_Simple_Line_Business_Logo_CGktk8RHK.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649962071315"/>
+            </div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                {heading}
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600 mt-5">
+            {paragraph} {' '}
+            <Link to={linkUrl} className="font-medium text-purple-600 hover:text-purple-500">
+                {linkName}
+            </Link>
+            </p>
         </div>
-    );
+    )
 }
