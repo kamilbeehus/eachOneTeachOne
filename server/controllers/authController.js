d3e42754a513dcf2e5b68ce143e27f2c2b320088
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-08-21 11:36:17 +0200

    Implemented the login controller and updated the signup controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,27 +1,71 @@
-import { signupService } from "../services/authService.js";
-import { generateToken } from "../utils/SecretToken.js";
+import { signup, login } from "../services/authService.js";
+import {
+  EmailAlreadyInUseError,
+  AuthenticationError,
+  ValidationError,
+} from "../errors/customErrors.js";
 
-export const signup = async (req, res) => {
+export const signupController = async (req, res) => {
   try {
-    const newUser = await signupService(req.body);
-    // Generate a JWT token using the user's information
-    const token = generateToken(newUser);
+    const { firstName, lastName, email, password } = req.body;
+
+    if (!firstName || !lastName || !email || !password) {
+      throw new ValidationError("All required fields must be provided.");
+    }
+
+    // Call the signup service to create a new user and generate a token
+    const { user, token } = await signup({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
 
     return res.status(201).json({
       message: "User registered successfully",
-      newUser,
+      user,
       token, // Include the token in the response
     });
   } catch (error) {
-    console.error("Error during user registration:", error);
-
-    if (error.name === "ValidationError") {
-      return res
-        .status(400)
-        .json({ message: "Validation error", error: error.message });
+    if (error instanceof EmailAlreadyInUseError) {
+      return res.status(400).json({ message: error.message });
     }
-    return res
-      .status(500)
-      .json({ message: "Server error", error: error.message });
+
+    if (error instanceof ValidationError) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    console.error("Signup error:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new ValidationError("All required fields must be provided.");
+    }
+
+    // Call the login service to authenticate the user and generate a token
+    const { user, token } = await login({ email, password });
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    if (error instanceof AuthenticationError) {
+      return res.status(401).json({ message: error.message });
+    }
+
+    if (error instanceof ValidationError) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    console.error("Login error:", error);
+    return res.status(500).json({ message: "Server error" });
   }
 };
