48af9d8d6b7e2976dd6ba9fc7dc39929e55cb0b9
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-08-21 11:34:53 +0200

    Implemented the login function and updated the signup function from the authService

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,39 +1,66 @@
 import User from "../models/User.js";
+import { generateToken } from "../utils/SecretToken.js";
+import {
+  EmailAlreadyInUseError,
+  AuthenticationError,
+  ValidationError,
+} from "../errors/customErrors.js";
 
-export const signupService = async ({
-  firstName,
-  lastName,
-  email,
-  passwordHash,
-}) => {
+export const signup = async ({ firstName, lastName, email, password }) => {
+  if (!firstName || !lastName || !email || !password) {
+    throw new ValidationError("All fields are required for signup.");
+  }
   // Check if the user already exists
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    throw new Error("Email already in use.");
+    throw new EmailAlreadyInUseError();
   }
 
   // Create a new User instance
-  const newUser = new User({
+  const user = new User({
     firstName,
     lastName,
     email,
-    passwordHash, // Password hashing should be handled by the User model
+    password, // Password hashing should be handled by the User model
   });
 
-  // Save the new user to the database
-  await newUser.save();
+  // Save the user to the database
+  await user.save();
 
-  // Prepare the user response object
-  const userResponse = {
-    _id: newUser._id,
-    firstName: newUser.firstName,
-    lastName: newUser.lastName,
-    email: newUser.email,
-    bio: newUser.bio,
-    profilePicture: newUser.profilePicture,
-    credits: newUser.credits,
-    createdAt: newUser.createdAt,
-  };
+  // Generate a JWT token with the user's information
+  const token = generateToken(user);
 
-  return userResponse;
+  return { user: formatUserResponse(user), token };
 };
+
+export const login = async ({ email, password }) => {
+  if (!email || !password) {
+    throw new ValidationError("All fields are required for login.");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AuthenticationError();
+  }
+
+  const isPasswordValid = await user.comparePassword(password);
+  if (!isPasswordValid) {
+    throw new AuthenticationError();
+  }
+
+  const token = generateToken(user);
+
+  return { user: formatUserResponse(user), token };
+};
+
+// Utility function to format user response
+const formatUserResponse = (user) => ({
+  _id: user._id,
+  firstName: user.firstName,
+  lastName: user.lastName,
+  email: user.email,
+  bio: user.bio,
+  profilePicture: user.profilePicture,
+  credits: user.credits,
+  createdAt: user.createdAt,
+});
