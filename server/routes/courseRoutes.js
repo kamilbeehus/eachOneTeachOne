a99e7ad9afec5165a475fe848bd33236697daf1d
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-08-23 14:17:24 +0200

    Added the route for getting all courses

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -1,13 +1,18 @@
 import express from "express";
-import { createCourseController } from "../controllers/courseController.js";
+import {
+  getAllCoursesController,
+  createCourseController,
+} from "../controllers/courseController.js";
 import { validateCourse } from "../middlewares/validateCourse.js";
 import { authenticateUser } from "../middlewares/authenticateUser.js";
 
 const router = express.Router();
 
-// Middleware to ensure the user is authenticated
+// Middleware to ensure the user is authenticated before accessing the routes
 router.use(authenticateUser);
 
+// Get all courses (GET): api/courses/
+router.get("/", getAllCoursesController);
 // Create a new course (POST): api/courses/create
 router.post("/create", validateCourse, createCourseController);
 
