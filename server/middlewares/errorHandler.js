126fe377397c44951bbf17425a59489560bf479a
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-08-23 15:36:36 +0200

    Updated errorHandler and Secret Token utilities

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,11 +1,7 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
-
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-
-  res.status(statusCode).json({
+  res.status(err.statusCode || 500).json({
     success: false,
-    message,
+    message: err.message || "Internal Server Error",
   });
 };
