e9cdb625279ab6ee799361fef957617a17daf8c3
M0reThanMach1nery <ferdi@posteo.de>
2024-09-01 20:00:03 +0200

    connect course cards from the homepage to the backend

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -1,9 +1,10 @@
-import raccoonLogo from "../assets/Raccoon.svg";
+// import raccoonLogo from "../assets/Raccoon.svg";
+import raccoonLogo from "../assets/Avatar.png";
 
 export default function CourseCard({ courseName, courseDescription }) {
   return (
     <>
-      <div className="card  bg-base-100 shadow-xl lg:card-side">
+      <div className="card bg-base-100 shadow-xl max-w-64">
         <figure>
           {/* Todo: insert teachers profile photo */}
           <img src={raccoonLogo} alt="raccoon" />
@@ -11,8 +12,8 @@ export default function CourseCard({ courseName, courseDescription }) {
         <div className="card-body">
           <h2 className="card-title">{courseName}</h2>
           <p>{courseDescription}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Teach me</button>
+          <div className="card-actions justify-between">
+            <button className="btn btn-primary">Enroll</button>
           </div>
         </div>
       </div>
--- client/src/components/CourseCardRow.jsx
+++ client/src/components/CourseCardRow.jsx
@@ -1,25 +1,32 @@
 import CourseCard from "./CourseCard";
+import axios from "axios";
+
+const getCourses = async () => {
+  const response = await axios.get("http://localhost:3000/api/courses/", {
+    withCredentials: true,
+  });
+  console.clear();
+  console.log(response.data);
+  return response.data.courses;
+};
+const courseArray = await getCourses();
+console.log(
+  "%c courseArray ",
+  "background: #fa8334; color: #1b1b1b; padding: 4px; border-radius:4px; font-weight: bold;",
+  courseArray,
+);
 
 export default function CourseCardRow() {
   return (
     <>
-      <div className="card grid sm:grid-cols-4 grid-cols-2 gap-6">
-        <CourseCard
-          courseName="guitar lesson"
-          courseDescription="Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        />
-        <CourseCard
-          courseName="cooking"
-          courseDescription="Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        />
-        <CourseCard
-          courseName="dancing"
-          courseDescription="Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        />
-        <CourseCard
-          courseName="maths"
-          courseDescription="Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        />
+      <div className="grid content-center justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {courseArray.map((course) => (
+          <CourseCard
+            key={course._id}
+            courseName={course.title}
+            courseDescription={course.description}
+          />
+        ))}
       </div>
     </>
   );
