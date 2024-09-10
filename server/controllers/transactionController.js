15c5165dfdac3ce6df6a39204823ee6c382121ae
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-09-10 17:11:55 +0200

    Implemented get Transaction History Controller

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -1,1 +1,27 @@
-//TODO: Create a route responsible for retrieving all transaction history for a user or admin view.
+import { getTransactionHistoryByUserId } from "../services/transactionService.js";
+import {
+  UserNotFoundError,
+  TransactionHistoryNotFoundError,
+} from "../errors/customErrors.js";
+
+// Get transaction history for a user by user ID Controller
+export const getTransactionHistoryController = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+
+    const transactions = await getTransactionHistoryByUserId(userId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Transaction history fetched successfully",
+      transactions,
+    });
+  } catch (error) {
+    if (error instanceof UserNotFoundError) {
+      return res.status(error.statusCode).json({ message: error.message });
+    } else if (error instanceof TransactionHistoryNotFoundError) {
+      return res.status(error.statusCode).json({ message: error.message });
+    }
+    next(error);
+  }
+};
