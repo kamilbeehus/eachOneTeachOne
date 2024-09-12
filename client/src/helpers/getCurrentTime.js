c97610a1506369e87773c2b62ba5c9838118a9b4
M0reThanMach1nery <ferdi@posteo.de>
2024-09-12 14:12:24 +0200

    WIP: add first prototype of date/time picker to offer course form

---
+++ client/src/helpers/getCurrentTime.js
@@ -0,0 +1,14 @@
+export function getCurrentTime(timeOffsetInHours) {
+  const time = new Date();
+
+  // Add a time offset to the time object e.g. add 2 hours: 9am -> 11am
+  if (timeOffsetInHours) {
+    time.setHours(time.getHours() + timeOffsetInHours);
+    console.log(time);
+  }
+
+  const currentMinutes = String(time.getMinutes()).padStart(2, "0");
+  const currentHours = String(time.getHours()).padStart(2, "0");
+  const returnString = `${currentHours}:${currentMinutes}:00`;
+  return returnString;
+}
