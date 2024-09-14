6ce7d5db5ee140bc8a8857a9f941b6beca65497e
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-09-14 13:36:34 +0200

    Updated db config

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,6 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const MONGO_URI = process.env.MONGO_URI;
-    console.log(MONGO_URI);
     await mongoose.connect(MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
