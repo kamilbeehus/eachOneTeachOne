3ad3502e7a56e091ee49baf01610161b12a3b0e8
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-11-23 16:27:54 +0100

    Refactored id

--- client/src/components/OfferCourse.jsx
+++ client/src/components/OfferCourse.jsx
@@ -43,7 +43,7 @@ export default function OfferCourse({ isUserCourse, refreshCourses }) {
     },
   };
 
-  if (isUserCourse && userId) {
+  if (isUserCourse) {
     return (
       <>
         <button
